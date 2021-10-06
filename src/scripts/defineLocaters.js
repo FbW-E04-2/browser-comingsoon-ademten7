53464fb598834bd1aa5a3ae909d5fdecb736e979
ademten82 <ademten7@gmail.com>
2021-10-07 01:47:23 +0200

    added solutions

---
+++ src/scripts/defineLocaters.js
@@ -0,0 +1,7 @@
+let para1 = document.querySelector("#paragraph1");
+let para2 = document.querySelector("#paragraph2");
+let para3 = document.querySelector("#paragraph3");
+let para4 = document.querySelector("#paragraph4");
+let iTags = document.querySelectorAll("i");
+
+export { para1, para2, para3, para4, iTags };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,16 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
 import '../styles/main.scss';
+import moment from "moment";
+import { para1, para2, para3, para4 } from "./defineLocaters.js"
+import { setIntervalFunc, hoverEffect } from "./functions.js";
+
+
+setIntervalFunc();
+hoverEffect()
+
+
 // Import any additional modules you want to include below \/
+//setInterval(moment().format('MMMM Do YYYY, h:mm:ss, a'), 1000)
 
 
 // \/ All of your javascript should go here \/
-
